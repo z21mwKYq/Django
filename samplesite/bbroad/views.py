761adc0bb9c0f6818c54769d0fb3720e25ae0137
Vladimir <vladimir-i2005@yandex.ru>
2022-11-13 15:41:00 +0300

    13.11.2022_2

--- samplesite/bbroad/views.py
+++ samplesite/bbroad/views.py
@@ -6,7 +6,5 @@ from .models import Bb
 
 
 def index (request):
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.all()
+    return render(request, 'bbroad/index.html', {'bbs':bbs})
