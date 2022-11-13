d5c407c8acd1ff78ce6882bebf9c6a180256c452
Vladimir <vladimir-i2005@yandex.ru>
2022-11-13 11:55:06 +0300

    13112022_1

--- samplesite/bbroad/views.py
+++ samplesite/bbroad/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Bb
 
 # Create your views here.
 
 
 def index (request):
-    return HttpResponse('Здесь будет выведен список объявлений !!!')
+    s = 'Список объявлений\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
