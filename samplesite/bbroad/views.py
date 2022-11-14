78f31b15df384eb851785d626e5d54098fd30de4
Vladimir <vladimir-i2005@yandex.ru>
2022-11-14 18:41:50 +0300

    14.11.2022_2

--- samplesite/bbroad/views.py
+++ samplesite/bbroad/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Bb
+from .models import Rubric
 
 # Create your views here.
 
 
 def index (request):
     bbs = Bb.objects.all()
-    return render(request, 'bbroad/index.html', {'bbs':bbs})
+    rubrics = Rubric.objects.all()
+    return render(request, 'bbroad/index.html', {'bbs':bbs, 'rubrics':rubrics})
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric = rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context={'bbs':bbs, 'rubrics':rubrics, 'current_rubric':current_rubric}
+    return render(request, 'bbroad/by_rubric.html', context)
