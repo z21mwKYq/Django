8acf7facfb911a8d9856e393127a810cca1be633
Vladimir <vladimir-i2005@yandex.ru>
2022-11-14 15:41:20 +0300

    14.11.2022_1

--- samplesite/bbroad/models.py
+++ samplesite/bbroad/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Rubric(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=15, verbose_name='Рубрика')
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
+
+
 class Bb(models.Model):
+    def __str__(self):
+        return self.title
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
-class Meta:
-    verbose_name_plural = 'Объявления'
-    verbose_name = 'Объявление'
-    ordering = ['-published']
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
+
+
