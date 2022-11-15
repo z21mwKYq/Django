597a3399487099066845bff01490ea362fd50dc6
Vladimir <vladimir-i2005@yandex.ru>
2022-11-15 22:37:51 +0300

    15.11.2022_1

--- samplesite/bbroad/urls.py
+++ samplesite/bbroad/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', index)
+    path('add/', BbCreateView.as_view(), name = 'add'),
+    path('<int:rubric_id>/', by_rubric, name = 'rubric'),
+    path('', index, name = 'index')
 ]
 
 
--- samplesite/bbroad/views.py
+++ samplesite/bbroad/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import BbForm
+
 from .models import Bb
 from .models import Rubric
 
@@ -17,3 +21,14 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context={'bbs':bbs, 'rubrics':rubrics, 'current_rubric':current_rubric}
     return render(request, 'bbroad/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bbroad/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
